2b0e8a4ecea0a994091a85db5afdf3538a76caed
Gabriell Huver <gabriell.is.huver@gmail.com>
2020-02-26 09:57:02 -0300

    test fixes

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -3,9 +3,8 @@ const correios = require('../../index')
 
 describe("Status test", () => {
     test("it should status code 200", async () => {
-        var ret = await correios.sro.rastrearObjeto("lb277957266se")
+        var ret = await correios.sro.rastrearObjeto("teste")
         console.log(ret)
         expect(ret.status_code).toEqual(200);
-   
     });
 });
